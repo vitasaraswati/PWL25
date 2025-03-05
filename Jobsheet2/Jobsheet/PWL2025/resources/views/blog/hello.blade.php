3d125f0a732df3845344b7bdca911a47e49208d7
unknown <vitaekasaraswati16@gmail.com>
2025-03-05 20:00:16 +0700

    delete and add folder js2

--- Jobsheet2/Jobsheet/PWL2025/resources/views/blog/hello.blade.php
+++
@@ -1,7 +0,0 @@
-<!-- View pada resources/views/hello.blade.php -->
-<html>
-    <body>
-    <h1>Hello, {{ $name }}</h1>
-    <h1>You are {{ $occupation }}</h1> <!--penambahan parameter baru-->
-    </body>
-</html> 
