2515890899723380ed5abc136c9711000e2d0a12
unknown <vitaekasaraswati16@gmail.com>
2025-03-08 15:28:21 +0700

    reupdate js3 and pwl_pos

---
+++ Minggu3/Jobsheet3/app/Http/Controllers/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\HTTP\Request;
+use App\Models\UserModel;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        //tambah data user dengan Eloquent Model
+        $data = [
+        
+            'username' => 'customer-2',
+            'nama' => 'Pelanggan Kedua',
+            'password' => Hash::make('12345'),
+            'level_id' => 4
+         ];
+         UserModel::insert($data);//tambahkan data ke tabel user
+         
+        // coba akses model UserModel
+        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        return view('user', ['data' => $user]);
+    }
+}
