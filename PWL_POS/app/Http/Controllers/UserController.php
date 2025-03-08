9c2ec26b1dcfaf1738564469314c0c313758c460
unknown <vitaekasaraswati16@gmail.com>
2025-03-08 12:23:51 +0700

    JS 4 - modify UserController

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -12,15 +12,33 @@ class UserController extends Controller
     public function index()
     {
         //tambah data user dengan Eloquent Model
-        $data = [
+        // $data = [
         
-            // 'username' => 'customer-1',
-            // 'nama' => 'Pelanggan Pertama',
-            // 'password' => Hash::make('12345'),
-            // 'level_id' => 4
-         ];
-         UserModel::insert($data);//tambahkan data ke tabel user
+        //     // 'username' => 'customer-1',
+        //     // 'nama' => 'Pelanggan Pertama',
+        //     // 'password' => Hash::make('12345'),
+        //     // 'level_id' => 4
+        //  ];
+        //  UserModel::insert($data);//tambahkan data ke tabel user
          
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_dua',
+        //     'nama' => 'Manager 2',
+        //     'password' => Hash::make('12345'),
+        // ];
+
+        $data = [
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345'),
+        ];
+
+
+        UserModel::create($data);
+
+
         // coba akses model UserModel
         $user = UserModel::all(); // ambil semua data dari tabel m_user
         return view('user', ['data' => $user]);
