30b9241454fb05e268993ed7a7d347e97295418a
unknown <vitaekasaraswati16@gmail.com>
2025-03-08 16:04:22 +0700

    praktikum 2.1 retrieving single models

--- Minggu4/Jobsheet4/app/Http/Controllers/UserController.php
+++ Minggu4/Jobsheet4/app/Http/Controllers/UserController.php
@@ -11,34 +11,24 @@ class UserController extends Controller
 {
     public function index()
     {
-        // //tambah data user dengan Eloquent Model
-        // $data = [
-        
-        //     'username' => 'customer-2',
-        //     'nama' => 'Pelanggan Kedua',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 4
-        //  ];
-        //  UserModel::insert($data);//tambahkan data ke tabel user
-         
-        // $data = [
-        //     'level_id' => 2,
-        //     'username' => 'manager_dua',
-        //     'nama' => 'Manager 2',
-        //     'password' => Hash::make('12345'),
-        // ];
-        
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345'),
-        ];
-        
-        UserModel::create($data);
+        // find berdasarkan id 
+        // $user = UserModel::find(1); // find id1 untuk ditampilkan ke browser 
+        // return view('user', ['data' => $user]);
 
-        // coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
-        return view('user', ['data' => $user]);
+        // where
+        // $user = UserModel::where('level_id', 1) -> first(); // where id1 untuk ditampilkan ke browser 
+        // return view('user', ['data' => $user]);
+
+        //firstwhere
+        // $user = UserModel::firstWhere('level_id', 1) -> first(); // mencari data dimana kolom level_id = 1
+        // return view('user', ['data' => $user]);
+
+        // findOr untuk menampilkan field tertentu
+        $user = UserModel::findOr(id: 20, columns: ['username', 'nama'], callback: function(): never {
+            abort (code: 404);
+        });
+        
+        // return view(view: 'user', data: ['data' => $user]);
+
     }
 }
