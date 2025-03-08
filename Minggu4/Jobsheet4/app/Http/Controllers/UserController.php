841ca38f243f28207ee19fd370b8c2f558932f06
unknown <vitaekasaraswati16@gmail.com>
2025-03-08 16:20:36 +0700

    praktikum 2.2 - not found exceptions

--- Minggu4/Jobsheet4/app/Http/Controllers/UserController.php
+++ Minggu4/Jobsheet4/app/Http/Controllers/UserController.php
@@ -11,24 +11,14 @@ class UserController extends Controller
 {
     public function index()
     {
-        // find berdasarkan id 
-        // $user = UserModel::find(1); // find id1 untuk ditampilkan ke browser 
+
+        //PRAKTIKUM 2.1
+
+        // find or fail 
+        // $user = UserModel::findOrFail(1);
         // return view('user', ['data' => $user]);
 
-        // where
-        // $user = UserModel::where('level_id', 1) -> first(); // where id1 untuk ditampilkan ke browser 
-        // return view('user', ['data' => $user]);
-
-        //firstwhere
-        // $user = UserModel::firstWhere('level_id', 1) -> first(); // mencari data dimana kolom level_id = 1
-        // return view('user', ['data' => $user]);
-
-        // findOr untuk menampilkan field tertentu
-        $user = UserModel::findOr(id: 20, columns: ['username', 'nama'], callback: function(): never {
-            abort (code: 404);
-        });
-        
-        // return view(view: 'user', data: ['data' => $user]);
-
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        return view('user', ['data' => $user]);
     }
 }
