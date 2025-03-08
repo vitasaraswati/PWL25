170deb5ea56707790fba43d1688b3ca707323ea8
unknown <vitaekasaraswati16@gmail.com>
2025-03-08 17:29:37 +0700

    praktikum 2.4 retreiving or creating models

--- Minggu4/Jobsheet4/app/Http/Controllers/UserController.php
+++ Minggu4/Jobsheet4/app/Http/Controllers/UserController.php
@@ -11,11 +11,17 @@ class UserController extends Controller
 {
     public function index()
     {
-        //PRAKTIKUM 2.3
-         // Menghitung jumlah pengguna dengan level_id = 2
-         $jumlahPengguna = UserModel::where('level_id', 2)->count();
-
-         // Mengirim data ke view
-         return view('user', ['jumlahPengguna' => $jumlahPengguna]);
+        //PRAKTIKUM 2.4
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'level_id' => 2
+            ]
+        );
+        $user->password = Hash::make('12345');
+        $user->save();
+        
+        return view('user', ['data' => $user]);
     }
 } 
--- Minggu4/Jobsheet4/resources/views/user.blade.php
+++ Minggu4/Jobsheet4/resources/views/user.blade.php
@@ -7,10 +7,16 @@
     <h1>Data User</h1>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            <th>Jumlah Pengguna</th>
+            <td>ID</td>
+            <td>Username</td>
+            <td>Nama</td>
+            <td>ID Level Pengguna</td>
         </tr>
         <tr>
-            <td>{{ $jumlahPengguna }}</td>
+            <td>{{ $data->user_id }}</td>
+            <td>{{ $data->username }}</td>
+            <td>{{ $data->nama }}</td>
+            <td>{{ $data->level_id }}</td>
         </tr>
     </table>
 </body>
