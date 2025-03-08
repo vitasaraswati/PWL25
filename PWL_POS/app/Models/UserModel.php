d4dcc993852de27f626b65dc7a8205310a73daee
unknown <vitaekasaraswati16@gmail.com>
2025-03-08 12:25:34 +0700

    JS4 - modify UserModel

--- PWL_POS/app/Models/UserModel.php
+++ PWL_POS/app/Models/UserModel.php
@@ -11,4 +11,9 @@ class UserModel extends Model
 
     protected $table = 'm_user';       // Mendefinisikan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'user_id';  // Mendefinisikan primary key dari tabel yang digunakan
+
+    //JS 4
+    // protected $fillable = ['level_id', 'username', 'nama', 'password']; //before modif
+    protected $fillable = ['level_id', 'username', 'nama'];
+
 }
