ac7af55f295bd8b1a6e1ee09c6367dde8f31dcbe
unknown <vitaekasaraswati16@gmail.com>
2025-03-23 10:22:46 +0700

    feat: tugas1 - web layouting m_level

--- Minggu5.2/Jobsheet5.2/app/Models/LevelModel.php
+++ Minggu5.2/Jobsheet5.2/app/Models/LevelModel.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class LevelModel extends Model
 {
@@ -12,5 +13,10 @@ class LevelModel extends Model
     protected $table = 'm_level'; // Mendefinisikan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'level_id'; // Mendefinisikan primary key dari tabel yang digunakan
 
-    protected $fillable = ['level_kode', 'level_nama']; // Mendefinisikan kolom yang bisa diisi oleh user
+    protected $fillable = ['level_kode', 'level_nama']; // FK
+
+    public function users():HasMany
+    {
+        return $this->hasMany(UserModel::class, 'level_id', 'level_id');
+    }
 }
--- Minggu5.2/Jobsheet5.2/routes/web.php
+++ Minggu5.2/Jobsheet5.2/routes/web.php
@@ -49,4 +49,15 @@ Route::group(['prefix' => 'user'], function () {
     Route::get('/{id}/edit', [UserController::class, 'edit']); // menampilkan halaman form edit user
     Route::put('/{id}', [UserController::class, 'update']); // menyimpan perubahan data user
     Route::delete('/{id}', [UserController::class, 'destroy']); // menghapus data user
+}); 
+
+Route::group(['prefix' => 'level'], function () {
+    Route::get('/', [LevelController::class, 'index']); // menampilkan halaman awal level
+    Route::post("/list", [LevelController::class, 'list']); // menampilkan data level dalam bentuk json untuk datatables
+    Route::get('/create', [LevelController::class, 'create']); // menampilkan halaman form tambah level
+    Route::post('/', [LevelController::class, 'store']); // menyimpan data level baru
+    Route::get('/{id}', [LevelController::class, 'show']); // menampilkan detail level
+    Route::get('/{id}/edit', [LevelController::class, 'edit']); // menampilkan halaman form edit level
+    Route::put('/{id}', [LevelController::class, 'update']); // menyimpan perubahan data level
+    Route::delete('/{id}', [LevelController::class, 'destroy']); // menghapus data level
 });
