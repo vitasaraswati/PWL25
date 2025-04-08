c8c6a96ef52daf5bc27f27272ce19a2219d16440
unknown <vitaekasaraswati16@gmail.com>
2025-04-08 17:46:23 +0700

    Revert "feat: implement jobsheet 3 - 5.2"
    This reverts commit 39abe825efba78da7c730244610ef650ae55ecfa.

--- PWL_POS/app/Models/UserModel.php
+++ PWL_POS/app/Models/UserModel.php
@@ -4,8 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use App\Models\LevelModel;
 
 class UserModel extends Model
 {
@@ -13,11 +11,4 @@ class UserModel extends Model
 
     protected $table = 'm_user';       // Mendefinisikan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'user_id';  // Mendefinisikan primary key dari tabel yang digunakan
-
-    protected $fillable = ['username', 'nama', 'password', 'level_id'];
-
-    public function level(): BelongsTo
-    {
-        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
-    }
 }
--- PWL_POS/routes/web.php
+++ PWL_POS/routes/web.php
@@ -1,16 +1,10 @@
 <?php
 
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\LevelController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\TransactionController;
-use App\Http\Controllers\LevelController; //tugas1 m_level
-use App\Http\Controllers\KategoriController; //tugas2 m_kategori
-use App\Http\Controllers\BarangController; //tugas3 m_barang
-use App\Http\Controllers\SupplierController; //tugas4 m_supplier
-use App\Http\Controllers\WelcomeController; //praktikum 2 minggu5.2 no 5
-use App\Http\Controllers\UserController; //praktikum3 minggu5.2 no 2
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -22,81 +16,10 @@ use App\Http\Controllers\UserController; //praktikum3 minggu5.2 no 2
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// Route::get('/level', [LevelController::class, 'index']);
-// Route::get('/kategori', [KategoriController::class, 'index']);
-// Route::get('/user', [UserController::class, 'index']);
-
-// Route::get('/user/tambah', [UserController::class, 'tambah']);
-// Route::post('user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-// Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-// Route::put('user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-// Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
-
-
-//PRAKTIKUM 5.2 
-//praktikum 2 - no 5
-Route::get('/', [WelcomeController::class, 'index']);
-
-//praktikum 3 - no 2
-Route::group(['prefix' => 'user'], function () {
-    Route::get('/', [UserController::class, 'index']); // menampilkan halaman awal user
-    Route::post('/list', [UserController::class, 'list']); // menampilkan data user dalam bentuk json untuk datatables
-    Route::get('/create', [UserController::class, 'create']); // menampilkan halaman form tambah user
-    Route::post('/', [UserController::class, 'store']); // menyimpan data user baru
-    Route::get('/{id}', [UserController::class, 'show']); // menampilkan detail user
-    Route::get('/{id}/edit', [UserController::class, 'edit']); // menampilkan halaman form edit user
-    Route::put('/{id}', [UserController::class, 'update']); // menyimpan perubahan data user
-    Route::delete('/{id}', [UserController::class, 'destroy']); // menghapus data user
-}); 
-
-//tugas 1 m_level
-Route::group(['prefix' => 'level'], function () {
-    Route::get('/', [LevelController::class, 'index']); // menampilkan halaman awal level
-    Route::post("/list", [LevelController::class, 'list']); // menampilkan data level dalam bentuk json untuk datatables
-    Route::get('/create', [LevelController::class, 'create']); // menampilkan halaman form tambah level
-    Route::post('/', [LevelController::class, 'store']); // menyimpan data level baru
-    Route::get('/{id}', [LevelController::class, 'show']); // menampilkan detail level
-    Route::get('/{id}/edit', [LevelController::class, 'edit']); // menampilkan halaman form edit level
-    Route::put('/{id}', [LevelController::class, 'update']); // menyimpan perubahan data level
-    Route::delete('/{id}', [LevelController::class, 'destroy']); // menghapus data level
+Route::get('/', function () {
+    return view('welcome');
 });
 
-//tugas 2 m_kategori
-Route::group(['prefix' => 'kategori'], function () {
-    Route::get('/', [KategoriController::class, 'index']); // menampilkan halaman awal kategori
-    Route::post("/list", [KategoriController::class, 'list']); // menampilkan data kategori dalam bentuk json untuk datatables
-    Route::get('/create', [KategoriController::class, 'create']); // menampilkan halaman form tambah kategori
-    Route::post('/', [KategoriController::class, 'store']); // menyimpan data kategori baru
-    Route::get('/{id}', [KategoriController::class, 'show']); // menampilkan detail kategori
-    Route::get('/{id}/edit', [KategoriController::class, 'edit']); // menampilkan halaman form edit kategori
-    Route::put('/{id}', [KategoriController::class, 'update']); // menyimpan perubahan data kategori
-    Route::delete('/{id}', [KategoriController::class, 'destroy']); // menghapus data kategori
-});
-
-//tugas 3 m_barang
-Route::group(['prefix' => 'barang'], function () {
-    Route::get('/', [BarangController::class, 'index']); // menampilkan halaman awal barang
-    Route::post("/list", [BarangController::class, 'list']); // menampilkan data barang dalam bentuk json untuk datatables
-    Route::get('/create', [BarangController::class, 'create']); // menampilkan halaman form tambah barang
-    Route::post('/', [BarangController::class, 'store']); // menyimpan data barang baru
-    Route::get('/{id}', [BarangController::class, 'show']); // menampilkan detail barang
-    Route::get('/{id}/edit', [BarangController::class, 'edit']); // menampilkan halaman form edit barang
-    Route::put('/{id}', [BarangController::class, 'update']); // menyimpan perubahan data barang
-    Route::delete('/{id}', [BarangController::class, 'destroy']); // menghapus data barang
-});
-
-//tugas 4 m_supplier
-Route::group(['prefix' => 'supplier'], function () {
-    Route::get('/', [SupplierController::class, 'index']); // menampilkan halaman awal supplier
-    Route::post("/list", [SupplierController::class, 'list']); // menampilkan data supplier dalam bentuk json untuk datatables
-    Route::get('/create', [SupplierController::class, 'create']); // menampilkan halaman form tambah supplier
-    Route::post('/', [SupplierController::class, 'store']); // menyimpan data supplier baru
-    Route::get('/{id}', [SupplierController::class, 'show']); // menampilkan detail supplier
-    Route::get('/{id}/edit', [SupplierController::class, 'edit']); // menampilkan halaman form edit supplier
-    Route::put('/{id}', [SupplierController::class, 'update']); // menyimpan perubahan data supplier
-    Route::delete('/{id}', [SupplierController::class, 'destroy']); // menghapus data supplier
-});
+Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
