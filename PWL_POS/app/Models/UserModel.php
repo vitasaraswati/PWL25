39abe825efba78da7c730244610ef650ae55ecfa
unknown <vitaekasaraswati16@gmail.com>
2025-04-08 17:05:48 +0700

    feat: implement jobsheet 3 - 5.2

--- PWL_POS/app/Models/UserModel.php
+++ PWL_POS/app/Models/UserModel.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\LevelModel;
 
 class UserModel extends Model
 {
@@ -11,4 +13,11 @@ class UserModel extends Model
 
     protected $table = 'm_user';       // Mendefinisikan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'user_id';  // Mendefinisikan primary key dari tabel yang digunakan
+
+    protected $fillable = ['username', 'nama', 'password', 'level_id'];
+
+    public function level(): BelongsTo
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
 }
