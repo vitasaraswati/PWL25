b2737b93c53921577203c329d9cc0917bd9a80af
unknown <vitaekasaraswati16@gmail.com>
2025-04-08 19:23:10 +0700

    fix: implement js 3-5.2

--- PWL_POS/resources/views/layouts/template.blade.php
+++ PWL_POS/resources/views/layouts/template.blade.php
@@ -32,7 +32,7 @@
       <!-- Brand Logo -->
       <a href="{{ url('/') }}" class="brand-link">
         <img src="{{ asset('adminlte/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img">
-        <span class="brand-text font-weight-light">POS - Point of Sales</span>
+        <span class="brand-text font-weight-light">PWL - Starter Code</span>
         </a>
 
 <!-- Sidebar -->
