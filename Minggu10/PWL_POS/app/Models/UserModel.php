ac3567bd17e2b987843d226defde7fae4b8b1d80
unknown <vitaekasaraswati16@gmail.com>
2025-04-27 11:55:20 +0700

    feat(api): praktikum 1 restful api register

--- Minggu10/PWL_POS/app/Models/UserModel.php
+++ Minggu10/PWL_POS/app/Models/UserModel.php
@@ -6,11 +6,21 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use App\Models\LevelModel;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Foundation\Auth\User as Authenticatable; //implementasi class Authenticatable
 
-class UserModel extends Authenticatable
+class UserModel extends Authenticatable implements JWTSubject
 {
+
     use HasFactory;
+    
+    public function getJWTIdentifier(){
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims(){
+        return [];
+    }
 
     protected $table = 'm_user';       // Mendefinisikan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'user_id';  // Mendefinisikan primary key dari tabel yang digunakan
