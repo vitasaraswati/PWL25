f43a0f1c04e79db1fd5178592e2897d49073a673
unknown <vitaekasaraswati16@gmail.com>
2025-04-27 16:18:29 +0700

    feat(api): tugas 3 restful api data barang

--- Minggu10/PWL_POS/routes/api.php
+++ Minggu10/PWL_POS/routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Api\LogoutController;
 use App\Http\Controllers\Api\LevelController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\KategoriController;
+use App\Http\Controllers\Api\BarangController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use PhpOffice\PhpSpreadsheet\Worksheet\Row;
@@ -30,23 +31,30 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//API Level 
+//Route API Level
 Route::get('/levels', [LevelController::class, 'index']);
 Route::post('/levels', [LevelController::class, 'store']);
 Route::get('/levels/{level}', [LevelController::class, 'show']);
 Route::put('/levels/{level}', [LevelController::class, 'update']);
 Route::delete('/levels/{level}', [LevelController::class, 'destroy']);
 
-//API User
+//Route API User
 Route::get('/users', [UserController::class, 'index']);
 Route::post('/users', [UserController::class, 'store']);
 Route::get('/users/{user}', [UserController::class, 'show']);
 Route::put('/users/{user}', [UserController::class, 'update']);
 Route::delete('/users/{user}', [UserController::class, 'destroy']); 
 
-//API Kategori
+//Route API Kategori
 Route::get('/kategoris', [KategoriController::class, 'index']);
 Route::post('/kategoris', [KategoriController::class, 'store']); 
 Route::get('/kategoris/{kategori}', [KategoriController::class, 'show']);
 Route::put('/kategoris/{kategori}', [KategoriController::class, 'update']);
 Route::delete('/kategoris/{kategori}', [KategoriController::class, 'destroy']);
+
+//Route API Barang
+Route::get('/barangs', [BarangController::class, 'index']);
+Route::post('/barangs', [BarangController::class, 'store']); 
+Route::get('/barangs/{barang}', [BarangController::class, 'show']);
+Route::put('/barangs/{barang}', [BarangController::class, 'update']);
+Route::delete('/barangs/{barang}', [BarangController::class, 'destroy']);
