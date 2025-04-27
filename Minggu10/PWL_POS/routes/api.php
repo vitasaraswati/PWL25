579e447e6be36bac4ef080a4d4aa04bbcae4e794
unknown <vitaekasaraswati16@gmail.com>
2025-04-27 13:59:48 +0700

    feat(api): praktikum 4 restful api level

--- Minggu10/PWL_POS/routes/api.php
+++ Minggu10/PWL_POS/routes/api.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\LogoutController;
+use App\Http\Controllers\Api\LevelController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,3 +26,9 @@ Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/levels', [LevelController::class, 'index']);
+Route::post('/levels', [LevelController::class, 'store']);
+Route::get('/levels/{level}', [LevelController::class, 'show']);
+Route::put('/levels/{level}', [LevelController::class, 'update']);
+Route::delete('/levels/{level}', [LevelController::class, 'destroy']);
