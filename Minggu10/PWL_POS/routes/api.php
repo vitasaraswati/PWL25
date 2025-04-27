7759751b7e69575efcd9e9674fb8dfaaa4b5be3c
unknown <vitaekasaraswati16@gmail.com>
2025-04-27 14:42:57 +0700

    feat(api): tugas 1 restful api data user

--- Minggu10/PWL_POS/routes/api.php
+++ Minggu10/PWL_POS/routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\LogoutController;
 use App\Http\Controllers\Api\LevelController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -27,8 +28,16 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//API Level 
 Route::get('/levels', [LevelController::class, 'index']);
 Route::post('/levels', [LevelController::class, 'store']);
 Route::get('/levels/{level}', [LevelController::class, 'show']);
 Route::put('/levels/{level}', [LevelController::class, 'update']);
 Route::delete('/levels/{level}', [LevelController::class, 'destroy']);
+
+//API User
+Route::get('/users', [UserController::class, 'index']);
+Route::post('/users', [UserController::class, 'store']);
+Route::get('/users/{user}', [UserController::class, 'show']);
+Route::put('/users/{user}', [UserController::class, 'update']);
+Route::delete('/users/{user}', [UserController::class, 'destroy']); 
